1dcee237cb4f2c4d96213d4c6ebf93b3807525b5
hbk <anakyn@naver.com>
2026-02-24 17:19:27 +0900

    lasttt

--- shop/src/main/java/com/hbk/repository/MainVideoRepository.java
+++ shop/src/main/java/com/hbk/repository/MainVideoRepository.java
@@ -1,4 +1,12 @@
 package com.hbk.repository;
 
-public interface MainVideoRepository {
+import com.hbk.entity.MainVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MainVideoRepository extends JpaRepository<MainVideo, Long> {
+
+    /** ✅ 항상 1개 유지 구조: 최신 1개 가져오기 */
+    Optional<MainVideo> findTopByOrderByIdDesc();
 }
